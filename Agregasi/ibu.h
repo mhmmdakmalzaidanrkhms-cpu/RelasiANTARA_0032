ea7a569b9ae37e8816c5a96bf0d76141658e8315
mhmmdakmalzaidanrkhms <mhmmdakmalzaidanrkhms@gmail.com>
2026-08-11 10:33:47 +0700

    menambah class ibu dan diimplementasikan ke anak

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -2,4 +2,21 @@
 #define IBU_H
 #include <vector>
 
+class ibu {
+public:
+    string nama;
+    vector<anak*> daftar_anak;
+
+    ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+
+    void tambahAnak(anak*);
+    void cetakAnak();
+};
+
 #endif
